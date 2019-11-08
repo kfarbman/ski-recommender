b3ae4878bc1fa6a50ee543227c1300cf2c5bfb48
Greg Chase <gregorywchase@gmail.com>
2019-11-08 16:46:17 -0700

    Remove unused code

--- src/webscrape_trails.py
+++ src/webscrape_trails.py
@@ -133,7 +133,9 @@ if __name__ == '__main__':
 
     # Request data from all ski resorts
     lst_resort_data = []
+
     import pdb; pdb.set_trace()
+
     for url in tqdm(lst_resort_urls):
         df_resort = ws.make_tables(URL=url)
         df_resort["URL"] = url
@@ -167,60 +169,3 @@ if __name__ == '__main__':
         {"__NA__": "0"}).fillna(df_combined["Vertical Drop (ft)"])
     
     df_combined['Average Steepness'] = df_combined['Vertical Drop (ft)'].astype(float)/(5280*df_combined['distance'].astype(float))
-
-
-# loveland_greens = [word.encode('ascii','ignore').strip().decode('utf-8') for word in d['Loveland']['green']['Name']]
-# loveland_blues = [word.encode('ascii','ignore').strip().decode('utf-8') for word in d['Loveland']['blue']['Name']]
-# loveland_blacks = [word.encode('ascii','ignore').strip().decode('utf-8') for word in d['Loveland']['black']['Name']]
-# loveland_bbs = [word.encode('ascii','ignore').strip().decode('utf-8') for word in d['Loveland']['bb']['Name']]
-#
-# def get_trails_list(resort,level):
-#     if d[resort][level] is None:
-#         return []
-#     else:
-#         return [word.encode('ascii','ignore').strip().decode('utf-8') for word in d[resort][level]['Name']]
-
-
-#
-# def get_table(URL):
-#     content = requests.get(URL).content
-#
-#     soup = BeautifulSoup(content, "html.parser")
-#
-#     rows = soup.select('tr')
-#
-#     table_lst = []
-#     for row in rows:
-#         cell_lst = [cell for cell in row if cell != '\n']
-#         cell_lst = [cell.text for cell in cell_lst]
-#         table_lst.append(cell_lst)
-#
-#     ranking = pd.DataFrame(table_lst)
-#     column_names = [x.strip('\n') for x in table_lst[0]]
-#     ranking.columns = column_names
-#     ranking = ranking.drop(0)
-#     if len(ranking['Resort Name'][1]) == 1:
-#         ranking = ranking.drop(1)
-#     ranking['Last Updated'] = ranking['Resort Name'].apply(lambda x: x.split('\n')[3])
-#     ranking['Resort Location'] = ranking['Resort Name'].apply(lambda x: x.split('\n')[2])
-#     ranking['Resort Name'] = ranking['Resort Name'].apply(lambda x: x.split('\n')[1])
-#     ranking['User Rating'] = ranking['User Rating'].apply(lambda x: x.split('\n')[1:3])
-#     return ranking
-#
-# terrain = get_table(URL_RM_terrain)
-# mtn_stats = get_table(URL_RM_stats)
-#
-# terrain['Runs'] = terrain['Runs'].apply(lambda x: int(x.strip('\n').replace('/','')))
-# levels = ['Beginner', 'Intermediate', 'Advanced', 'Expert']
-# level_columns = dict()
-# for level in levels:
-#     terrain[level] = terrain[level].apply(lambda x: int(x[:-1]) if len(x) > 2 else 0)
-#     level_columns[level] = '% '+level
-# terrain = terrain.rename(columns = level_columns)
-#
-# num_fields = ['Base','Summit','Vertical Drop','Longest Run','Snow Making']
-# field_columns = dict()
-# for field in num_fields:
-#     field_columns[field] = field+' ({})'.format(mtn_stats[field][1][-2:])
-#     mtn_stats[field] = mtn_stats[field].apply(lambda x: float(x[:-2]) if x != 'N/A' else 0)
-# mtn_stats = mtn_stats.rename(columns=field_columns)
