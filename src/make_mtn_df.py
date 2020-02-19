96eb82c8e0a61db5419130c326be4fc68c746b0d
Greg Chase <gregorywchase@gmail.com>
2020-02-19 15:50:59 -0700

    Disable functions and objects (WIP)

--- src/make_mtn_df.py
+++ src/make_mtn_df.py
@@ -1,3 +1,12 @@
+"""
+Webscrape mountain statistics from OnTheSnow and SkiMap
+
+1. Request all mountain data
+2. Format/ preprocess data
+3. Combine mountain and elevation data
+4. Save data to Parquet file
+"""
+
 import pickle
 import warnings
 from tqdm import tqdm
@@ -16,37 +25,44 @@ class MakeMountainDF:
     def __init__(self):
         
         self.browser_options = webdriver.ChromeOptions()
-        self.browser_options.add_argument("headless")
+        self.browser_options.add_argument('--no-sandbox')
+        self.browser_options.add_argument('--window-size=1420,1080')
+        self.browser_options.add_argument('--headless')
+        self.browser_options.add_argument('--disable-gpu')
 
         self.browser = webdriver.Chrome(chrome_options=self.browser_options)
         
-        self.resort_urls = {
-                            'Alpine Meadows': 'california/squaw-valley-usa',
-                            'Arapahoe Basin': 'colorado/arapahoe-basin-ski-area',
-                            'Beaver Creek': 'colorado/beaver-creek',
-                            'Copper': 'colorado/copper-mountain-resort',
-                            'Crested Butte': 'colorado/crested-butte-mountain-resort',
-                            'Diamond Peak': 'nevada/diamond-peak',
-                            'Eldora': 'colorado/eldora-mountain-resort',
-                            'Loveland': 'colorado/loveland',
-                            'Monarch': 'colorado/monarch-mountain',
-                            'Taos': 'new-mexico/taos-ski-valley',
-                            'Vail': 'colorado/vail',
-                            'Winter Park': 'colorado/winter-park-resort'}
+        # self.resort_urls = {
+        #                     'Alpine Meadows': 'california/squaw-valley-usa',
+        #                     'Arapahoe Basin': 'colorado/arapahoe-basin-ski-area',
+        #                     # TODO: Aspen Snowmass
+        #                     # TODO: Bald Mountain
+        #                     'Beaver Creek': 'colorado/beaver-creek',
+        #                     'Copper': 'colorado/copper-mountain-resort',
+        #                     'Crested Butte': 'colorado/crested-butte-mountain-resort',
+        #                     'Diamond Peak': 'nevada/diamond-peak',
+        #                     'Eldora': 'colorado/eldora-mountain-resort',
+        #                     'Loveland': 'colorado/loveland',
+        #                     'Monarch': 'colorado/monarch-mountain',
+        #                     # TODO: Steamboat
+        #                     'Taos': 'new-mexico/taos-ski-valley',
+        #                     # TODO: Telluride
+        #                     'Vail': 'colorado/vail',
+        #                     'Winter Park': 'colorado/winter-park-resort'}
         
-        self.resort_elevation = {
-                            'Alpine Meadows': 522,
-                            'Arapahoe Basin': 513,
-                            'Beaver Creek': 497,
-                            'Copper': 509,
-                            'Crested Butte': 514,
-                            # 'Diamond Peak': 359,
-                            'Eldora': 508,
-                            'Loveland': 515,
-                            'Monarch': 511,
-                            # 'Taos': 338,
-                            'Vail': 507,
-                            'Winter Park': 503}
+        # self.resort_elevation = {
+        #                     'Alpine Meadows': 522,
+        #                     'Arapahoe Basin': 513,
+        #                     'Beaver Creek': 497,
+        #                     'Copper': 509,
+        #                     'Crested Butte': 514,
+        #                     # 'Diamond Peak': 359,
+        #                     'Eldora': 508,
+        #                     'Loveland': 515,
+        #                     'Monarch': 511,
+        #                     # 'Taos': 338,
+        #                     'Vail': 507,
+        #                     'Winter Park': 503}
         
         # 2020 ticket prices, fetched manually
         self.dict_resort_prices = {'Alpine Meadows': 169,
@@ -62,100 +78,100 @@ class MakeMountainDF:
                                    'Vail': 209,
                                    'Winter Park': 139}
 
-    def get_resort_terrain(self, resort):
-        """
-        Request elevation, run colors, chairlifts, and prices from each resort
-        """
+    # def get_resort_terrain(self, resort):
+    #     """
+    #     Request elevation, run colors, chairlifts, and prices from each resort
+    #     """
         
-        URL = f'http://www.onthesnow.com/{self.resort_urls[resort]}/ski-resort.html'
+    #     URL = f'http://www.onthesnow.com/{self.resort_urls[resort]}/ski-resort.html'
                 
-        self.browser.get(URL)
+    #     self.browser.get(URL)
         
-        soup = BeautifulSoup(self.browser.page_source, 'html.parser')
+    #     soup = BeautifulSoup(self.browser.page_source, 'html.parser')
 
-        lst_terrain = soup.select('div#resort_terrain p')
-        lst_terrain = [i.get_text() for i in lst_terrain]
+    #     lst_terrain = soup.select('div#resort_terrain p')
+    #     lst_terrain = [i.get_text() for i in lst_terrain]
 
-        # terrain types
-        lst_terrain_type = lst_terrain[::2]
+    #     # terrain types
+    #     lst_terrain_type = lst_terrain[::2]
 
-        # terrain figures and numbers
-        lst_terrain_figures = lst_terrain[1::2]
+    #     # terrain figures and numbers
+    #     lst_terrain_figures = lst_terrain[1::2]
         
-        df_terrain = pd.DataFrame({'type': lst_terrain_type, 'figures': lst_terrain_figures})
+    #     df_terrain = pd.DataFrame({'type': lst_terrain_type, 'figures': lst_terrain_figures})
 
-        df_terrain["type"] = df_terrain["type"].str.replace(" Runs", "")
+    #     df_terrain["type"] = df_terrain["type"].str.replace(" Runs", "")
         
-        df_terrain["resort"] = resort
+    #     df_terrain["resort"] = resort
         
-        return df_terrain
+    #     return df_terrain
 
-    def get_resort_elevation_and_lifts(self, resort):
-        """
-        Request elevation, total runs, and total lifts per resort
-        """
+    # def get_resort_elevation_and_lifts(self, resort):
+    #     """
+    #     Request elevation, total runs, and total lifts per resort
+    #     """
         
-        URL = f"https://skimap.org/SkiAreas/view/{self.resort_elevation[resort]}.json"
+    #     URL = f"https://skimap.org/SkiAreas/view/{self.resort_elevation[resort]}.json"
                 
-        json_resort = requests.get(URL).json()
+    #     json_resort = requests.get(URL).json()
 
-        dict_resort_elevation = {}
+    #     dict_resort_elevation = {}
 
-        dict_resort_elevation["resort"] = json_resort["name"]
-        dict_resort_elevation["lifts"] = json_resort["lift_count"]
-        dict_resort_elevation["run_count"] = json_resort["run_count"]
+    #     dict_resort_elevation["resort"] = json_resort["name"]
+    #     dict_resort_elevation["lifts"] = json_resort["lift_count"]
+    #     dict_resort_elevation["run_count"] = json_resort["run_count"]
         
-        # Meters: Multipy by 3.281 to get feet
-        dict_resort_elevation["top_elevation"] = int(round(json_resort["top_elevation"] * 3.281))
-        dict_resort_elevation["bottom_elevation"] = int(round(json_resort["bottom_elevation"] * 3.281))
+    #     # Meters: Multipy by 3.281 to get feet
+    #     dict_resort_elevation["top_elevation"] = int(round(json_resort["top_elevation"] * 3.281))
+    #     dict_resort_elevation["bottom_elevation"] = int(round(json_resort["bottom_elevation"] * 3.281))
 
-        return dict_resort_elevation
+    #     return dict_resort_elevation
 
-    def create_mountain_data_frame(self):
-        """
-        Run get_resort_terrain
+    # def create_mountain_data_frame(self):
+    #     """
+    #     Run get_resort_terrain
 
-        Output
-            Pandas DataFrame of terrain data per resort
-        """
-        lst_resorts = []
+    #     Output
+    #         Pandas DataFrame of terrain data per resort
+    #     """
+    #     lst_resorts = []
 
-        for resort in tqdm(self.resort_urls.keys()):
-            lst_resorts.append(self.get_resort_terrain(resort))
+    #     for resort in tqdm(self.resort_urls.keys()):
+    #         lst_resorts.append(self.get_resort_terrain(resort))
         
-        # Combine list of resort DataFrames
-        df_terrain = pd.concat(lst_resorts).reset_index(drop=True)
+    #     # Combine list of resort DataFrames
+    #     df_terrain = pd.concat(lst_resorts).reset_index(drop=True)
 
-        return df_terrain
+    #     return df_terrain
 
-    def create_elevation_data_frame(self):
-        """
-        Run get_resort_elevation_and_lifts
-        Append missing resort data
-        """
+    # def create_elevation_data_frame(self):
+    #     """
+    #     Run get_resort_elevation_and_lifts
+    #     Append missing resort data
+    #     """
         
-        lst_resorts = [self.get_resort_elevation_and_lifts(resort)
-                   for resort in tqdm(self.resort_elevation)]
+    #     lst_resorts = [self.get_resort_elevation_and_lifts(resort)
+    #                for resort in tqdm(self.resort_elevation)]
         
-        # Create missing values from skimap website
-        dict_diamond_peak = {"resort": "Diamond Peak",
-                            "lifts": 7,
-                            "run_count": 30,
-                            "top_elevation": 8540,
-                            "bottom_elevation": 6700}
+    #     # Create missing values from skimap website
+    #     dict_diamond_peak = {"resort": "Diamond Peak",
+    #                         "lifts": 7,
+    #                         "run_count": 30,
+    #                         "top_elevation": 8540,
+    #                         "bottom_elevation": 6700}
         
-        dict_taos = {"resort": "Taos",
-                            "lifts": 14,
-                            "run_count": 110,
-                            "top_elevation": 12481,
-                            "bottom_elevation": 9200}
+    #     dict_taos = {"resort": "Taos",
+    #                         "lifts": 14,
+    #                         "run_count": 110,
+    #                         "top_elevation": 12481,
+    #                         "bottom_elevation": 9200}
 
-        lst_resorts.append(dict_diamond_peak)
-        lst_resorts.append(dict_taos)
+    #     lst_resorts.append(dict_diamond_peak)
+    #     lst_resorts.append(dict_taos)
 
-        df_elevations = pd.DataFrame(lst_resorts)
+    #     df_elevations = pd.DataFrame(lst_resorts)
 
-        return df_elevations
+    #     return df_elevations
 
     def format_mountain_data_frame_values(self, df):
         """
