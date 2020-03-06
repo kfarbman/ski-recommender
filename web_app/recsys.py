f3a694b9a3dd9f0c1d31349690798888e12ca148
Greg Chase <gregorywchase@gmail.com>
2020-03-06 14:09:40 -0700

    Remove Flask code

--- web_app/recsys.py
+++ web_app/recsys.py
@@ -110,6 +110,7 @@ class SkiRunRecommender:
         return resort_stats_df
 
     # TODO: Simplify syntax
+    # TODO: Add dummied features for additional data
     def mountain_recommendations(self, index, n=5):
         """
         Create mountain recommendations
@@ -137,7 +138,8 @@ class SkiRunRecommender:
         
         return orig_row, list(df_sorted_recs.index[:n])
 
-    # TODO: Simplify syntax 
+    # TODO: Simplify syntax
+    # TODO: Add dummied features for additional data
     def trail_recommendations(self, index, n=5, resort=None, color=None):
         """
         Cosine similarity recommendations
@@ -204,100 +206,7 @@ class SkiRunRecommender:
         # Max Grade
         return df
     
-# @app.route('/', methods =['GET','POST'])    
-# def index():
-#     return render_template('home.html')
-
-# # TODO: Correct df input
-# @app.route('/trails', methods=['GET','POST'])
-# def trails():
-#     return render_template('index.html',df=df)
-    
-# # TODO: Correct df input
-# @app.route('/mountains', methods=['GET','POST'])
-# def mountains():
-#     return render_template('mtn_index.html',df=df)
-
-# @app.route('/recommendations', methods=['GET','POST'])
-# def recommendations():
-#     color_lst = None
-
-#     dict_run_requests = {"green": ["green"],
-#                         "blue": ["green", "blue"],
-#                         "black": ["green", "blue", "black"],
-#                         "bb": ["green", "blue", "black", "bb"]}
-
-#     request_difficulty = "max_difficulty"
-#     color_lst = request.form.get(dict_run_requests[request_difficulty])
-
-#     # if request.form.get('green'):
-#     #     color_lst = ['green']
-#     # if request.form.get('blue'):
-#     #     color_lst = ['green','blue']
-#     # if request.form.get('black'):
-#     #     color_lst = ['green','blue','black']
-#     # if request.form.get('bb'):
-#     #     color_lst = ['green','blue','black','bb']
-    
-#     # CHECKBOX FUNCTIONALITY!!!
-#     resort = request.form['resort']
-#     if resort == '':
-#         return 'You must select a trail from your favorite resort.'
-#     trail = request.form['trail']
-#     if trail != '':
-#         index = int(trail)
-#         dest_resort = request.form['dest_resort']
-#         num_recs = int(request.form['num_recs'])
-#         rec_df = self.cos_sim_recs(index,num_recs,dest_resort,color_lst)
-#         rec_df = self.clean_df_for_recs(rec_df)
-#         if dest_resort == '':
-#             resort_links = self.links[resort]
-#         else:
-#             resort_links = self.links[dest_resort]
-#         return render_template('recommendations.html',rec_df=rec_df,resort_links=resort_links)
-#     return 'You must select a trail.'
-    
-# # TODO: Correct inputs
-# @app.route('/mtn_recommendations', methods=['GET','POST'])
-# def mtn_recommendations(self):
-#     resort = request.form['resort']
-#     if resort == '':
-#         return 'You must select a trail from your favorite resort.'
-#     trail = request.form['trail']
-#     if trail != '':
-#         index = int(trail)
-#         num_recs = int(request.form['num_recs'])
-#         row, recs = self.mtn_recommender(index,num_recs)
-#         results_df = pd.DataFrame(columns=['resort', 'resort_bottom','resort_top','greens','blues','blacks','bbs','lifts','price'])
-#         for rec in recs:
-#             results_df = results_df.append(resort_stats_df[resort_stats_df['resort'] == rec])
-#         row = self.clean_df_for_recs(row)
-#         results_df.drop('Price', axis=1, inplace=True)
-#         results_df.columns = ['Resort','Bottom Elevation (ft)', 'Top Elevation (ft)', 'Percent Greens', 'Percent Blues', 'Percent Blacks', 'Percent Double  Blacks', 'Number of Lifts']
-#         return render_template('mtn_recommendations.html',row=row,results_df=results_df,links=links)
-#     return 'You must select a trail.'
-
-# @app.route('/get_trails')
-# def get_trails(self):
-#     resort = request.args.get('resort')
-#     if resort:
-#         df = self.load_trail_data()
-#         sub_df = df[df['resort'] == resort]
-#         sub_df['trail_name'] = sub_df['trail_name'].apply(lambda x: x.split()).apply(lambda x: (x[1:] + ['Upper']) if (x[0] == 'Upper') else x).apply(lambda x: ' '.join(x))
-#         sub_df['trail_name'] = sub_df['trail_name'].apply(lambda x: x.split()).apply(lambda x: (x[1:] + ['Lower']) if (x[0] == 'Lower') else x).apply(lambda x: ' '.join(x))
-#         sub_df.sort_values(by='trail_name',inplace=True)
-#         id_name_color = [("","Select a Trail...","white")] + list(zip(list(sub_df.index),list(sub_df['trail_name']),list(sub_df['colors'])))
-#         data = [{"id": str(x[0]), "name": x[1], "color": x[2]} for x in id_name_color]
-#         # print(data)
-#     return jsonify(data)
-
-# @app.route('/trail_map/<resort>')
-# def trail_map(self, resort):
-#     resort_image = self.links[resort][0]
-#     return render_template('trail_map.html',resort_image=resort_image)
-    
 if  __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
     
     recsys = SkiRunRecommender()
 
