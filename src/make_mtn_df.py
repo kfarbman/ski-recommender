d52d9e71bf55369807c0a314b301056cb968161c
Greg Chase <9953173+gregwchase@users.noreply.github.com>
2021-03-15 17:28:32 -0600

    Black linter formatting

--- src/make_mtn_df.py
+++ src/make_mtn_df.py
@@ -20,63 +20,64 @@ from tqdm import tqdm
 
 from src.webscrape_trails import WebscrapeTrails
 
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
 
 
 class MakeMountainDF:
-
     def __init__(self):
 
         self.CURRENT_DIRECTORY = os.getcwd()
 
         self.browser_options = webdriver.ChromeOptions()
-        self.browser_options.add_argument('--no-sandbox')
-        self.browser_options.add_argument('--headless')
-        self.browser_options.add_argument('--disable-gpu')
+        self.browser_options.add_argument("--no-sandbox")
+        self.browser_options.add_argument("--headless")
+        self.browser_options.add_argument("--disable-gpu")
 
         self.browser = webdriver.Chrome(options=self.browser_options)
 
         # 2020 ticket prices, fetched manually
-        self.dict_resort_prices = {'Alpine Meadows': 169,
-                                   'Arapahoe Basin': 109,
-                                   'Aspen Snowmass': 179,
-                                   'Bald Mountain': 145,
-                                   'Beaver Creek': 209,
-                                   'Copper': 119,
-                                   'Crested Butte': 129,
-                                   'Diamond Peak': 104,
-                                   'Eldora': 140,
-                                   'Jackson Hole': 165,
-                                   'Loveland': 89,
-                                   'Monarch': 94,
-                                   'Steamboat': 199,
-                                   'Taos': 110,
-                                   'Telluride': 149,
-                                   'Vail': 209,
-                                   'Winter Park': 139,
-                                   'Wolf Creek': 76}
+        self.dict_resort_prices = {
+            "Alpine Meadows": 169,
+            "Arapahoe Basin": 109,
+            "Aspen Snowmass": 179,
+            "Bald Mountain": 145,
+            "Beaver Creek": 209,
+            "Copper": 119,
+            "Crested Butte": 129,
+            "Diamond Peak": 104,
+            "Eldora": 140,
+            "Jackson Hole": 165,
+            "Loveland": 89,
+            "Monarch": 94,
+            "Steamboat": 199,
+            "Taos": 110,
+            "Telluride": 149,
+            "Vail": 209,
+            "Winter Park": 139,
+            "Wolf Creek": 76,
+        }
 
     def get_mountain_data(self, URL: str) -> pd.core.frame.DataFrame:
-        '''
+        """
         Inputs:
             URL from URLs (str)
         Outputs:
             Pandas DataFrame of ski resort information
-        '''
+        """
 
         self.browser.get(URL)
 
         time.sleep(3)
 
-        soup = BeautifulSoup(self.browser.page_source, 'html.parser')
+        soup = BeautifulSoup(self.browser.page_source, "html.parser")
 
         # JollyTurns parsing (runs breakdown)
         X_runs = soup.select(
-            "resort-glance div.row div.col-xs-12 div.row.text-left.statistics.ng-scope span.ng-binding")
+            "resort-glance div.row div.col-xs-12 div.row.text-left.statistics.ng-scope span.ng-binding"
+        )
         lst_runs = [run.text for run in X_runs]
         lst_runs = [run.replace(" ski runs: ", "") for run in lst_runs]
-        df_ski_runs = pd.DataFrame(
-            {"Runs": lst_runs[0::2], "total": lst_runs[1::2]})
+        df_ski_runs = pd.DataFrame({"Runs": lst_runs[0::2], "total": lst_runs[1::2]})
         df_ski_runs = df_ski_runs.set_index("Runs").T.reset_index(drop=True)
 
         # JollyTurns parsing (Chairlifts / total runs)
@@ -85,21 +86,20 @@ class MakeMountainDF:
         df_lifts = pd.DataFrame({"Lifts": lst_lifts[0]}, index=[0])
 
         # JollyTurns parsing (Elevations)
-        X_elevations = soup.select(
-            "resort-glance div.row div.col-xs-12 table tr td")
+        X_elevations = soup.select("resort-glance div.row div.col-xs-12 table tr td")
         lst_elevations = [
-            elevation.text for elevation in X_elevations if "Lift" not in elevation.text]
-        lst_elevations = [elevation.replace(
-            " \xa0", "") for elevation in lst_elevations]
-        lst_elevations = [elevation.replace(
-            " ft", "") for elevation in lst_elevations]
-        lst_elevations = [elevation.replace(
-            ":", "") for elevation in lst_elevations]
+            elevation.text for elevation in X_elevations if "Lift" not in elevation.text
+        ]
+        lst_elevations = [
+            elevation.replace(" \xa0", "") for elevation in lst_elevations
+        ]
+        lst_elevations = [elevation.replace(" ft", "") for elevation in lst_elevations]
+        lst_elevations = [elevation.replace(":", "") for elevation in lst_elevations]
 
         df_elevations = pd.DataFrame(
-            {"Elevation": lst_elevations[0::2], "Total": lst_elevations[1::2]})
-        df_elevations = df_elevations.set_index(
-            "Elevation").T.reset_index(drop=True)
+            {"Elevation": lst_elevations[0::2], "Total": lst_elevations[1::2]}
+        )
+        df_elevations = df_elevations.set_index("Elevation").T.reset_index(drop=True)
 
         # Combine total runs, total lifts, and elevation data
         df_ski = pd.concat([df_ski_runs, df_lifts, df_elevations], axis=1)
@@ -108,7 +108,9 @@ class MakeMountainDF:
 
         return df_ski
 
-    def format_mountain_data_frame_values(self, df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
+    def format_mountain_data_frame_values(
+        self, df: pd.core.frame.DataFrame
+    ) -> pd.core.frame.DataFrame:
         """
         Pivot DataFrame, and format values
 
@@ -119,8 +121,17 @@ class MakeMountainDF:
             Formatted Pandas DataFrame
         """
 
-        lst_columns = ["Top", "Base", "Lifts", "Vertical rise",
-                       "black", "blue", "double black", "green", "terrain park"]
+        lst_columns = [
+            "Top",
+            "Base",
+            "Lifts",
+            "Vertical rise",
+            "black",
+            "blue",
+            "double black",
+            "green",
+            "terrain park",
+        ]
 
         df[lst_columns] = df[lst_columns].fillna(0)
 
@@ -128,7 +139,9 @@ class MakeMountainDF:
 
         return df
 
-    def save_mountain_data(self, df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
+    def save_mountain_data(
+        self, df: pd.core.frame.DataFrame
+    ) -> pd.core.frame.DataFrame:
         """
         Save formatted mountain data to Parquet file
         """
@@ -136,18 +149,19 @@ class MakeMountainDF:
         current_date = date.today().strftime("%Y%m%d")
 
         df.to_parquet(
-            f"{self.CURRENT_DIRECTORY}/data/mountain_data_{current_date}.parquet", index=False)
+            f"{self.CURRENT_DIRECTORY}/data/mountain_data_{current_date}.parquet",
+            index=False,
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     mountain = MakeMountainDF()
 
     ws = WebscrapeTrails()
 
     # Request mountain data from all resorts
-    lst_mountain_data = [mountain.get_mountain_data(
-        URL=url) for url in tqdm(ws.URLs)]
+    lst_mountain_data = [mountain.get_mountain_data(URL=url) for url in tqdm(ws.URLs)]
 
     # Combine mountain data
     df_mountain = pd.concat(lst_mountain_data).reset_index(drop=True)
@@ -155,8 +169,7 @@ if __name__ == '__main__':
     df_mountain = ws.rename_resorts(df=df_mountain)
 
     # Fill prices
-    df_mountain["Price"] = df_mountain["Resort"].map(
-        mountain.dict_resort_prices)
+    df_mountain["Price"] = df_mountain["Resort"].map(mountain.dict_resort_prices)
 
     # Convert column data types
     df_mountain = mountain.format_mountain_data_frame_values(df=df_mountain)
@@ -164,18 +177,26 @@ if __name__ == '__main__':
     # Convert total runs to percentage of total runs per resort
     lst_run_types = ["black", "blue", "double black", "green", "terrain park"]
     df_mountain["Total Runs"] = df_mountain[lst_run_types].sum(axis=1)
-    df_mountain[lst_run_types] = df_mountain[lst_run_types].div(
-        df_mountain["Total Runs"], axis=0).mul(100).round().astype(int)
+    df_mountain[lst_run_types] = (
+        df_mountain[lst_run_types]
+        .div(df_mountain["Total Runs"], axis=0)
+        .mul(100)
+        .round()
+        .astype(int)
+    )
 
     # Rename columns
-    df_mountain.rename(columns={
-        "Vertical rise": "Vertical Rise (ft)",
-        "black": "Percent Blacks",
-        "blue": "Percent Blues",
-        "double black": "Percent Double Blacks",
-        "green": "Percent Greens",
-        "terrain park": "Percent Terrain Parks"
-    }, inplace=True)
+    df_mountain.rename(
+        columns={
+            "Vertical rise": "Vertical Rise (ft)",
+            "black": "Percent Blacks",
+            "blue": "Percent Blues",
+            "double black": "Percent Double Blacks",
+            "green": "Percent Greens",
+            "terrain park": "Percent Terrain Parks",
+        },
+        inplace=True,
+    )
 
     # Save data
     # mountain.save_mountain_data(df=df_mountain)
--- src/webscrape_trails.py
+++ src/webscrape_trails.py
@@ -14,7 +14,6 @@ from tqdm import tqdm
 
 
 class WebscrapeTrails:
-
     def __init__(self):
 
         self.CURRENT_DIRECTORY = os.getcwd()
@@ -58,53 +57,70 @@ class WebscrapeTrails:
             self.TELLURIDE_URL,
             self.VAIL_URL,
             self.WINTER_PARK_URL,
-            self.WOLF_CREEK_URL]
+            self.WOLF_CREEK_URL,
+        ]
 
         self.browser_options = webdriver.ChromeOptions()
-        self.browser_options.add_argument('--no-sandbox')
-        self.browser_options.add_argument('--headless')
-        self.browser_options.add_argument('--disable-gpu')
+        self.browser_options.add_argument("--no-sandbox")
+        self.browser_options.add_argument("--headless")
+        self.browser_options.add_argument("--disable-gpu")
 
         self.browser = webdriver.Chrome(options=self.browser_options)
 
-        self.lst_run_difficulty = ["skiruns-green", "skiruns-blue", "skiruns-black", "skiruns-double-black"]
+        self.lst_run_difficulty = [
+            "skiruns-green",
+            "skiruns-blue",
+            "skiruns-black",
+            "skiruns-double-black",
+        ]
 
         self.blank_value = "__NA__"
 
     def make_tables(self, URL: str) -> pd.core.frame.DataFrame:
-        '''
+        """
         Inputs:
             URL from URLs (str)
         Outputs:
             Pandas DataFrame of ski resort information
-        '''       
-        
+        """
+
         self.browser.get(URL)
 
         time.sleep(3)
 
-        soup = BeautifulSoup(self.browser.page_source, 'html.parser')
+        soup = BeautifulSoup(self.browser.page_source, "html.parser")
 
-        X_web_trail = soup.select('table.table-striped tr')
+        X_web_trail = soup.select("table.table-striped tr")
 
         lst_rows = [x.text.strip() for x in X_web_trail]
         lst_rows = [i.replace("  ", "|") for i in lst_rows]
         lst_rows = [i.replace(" ft", "|") for i in lst_rows]
         lst_rows = [i.replace(" mi", "|") for i in lst_rows]
 
-        lst_cols = ['Name Bottom Top Vertical rise',
-                    'Name Bottom Top Vertical drop Length',
-                    'Name Elevation']
+        lst_cols = [
+            "Name Bottom Top Vertical rise",
+            "Name Bottom Top Vertical drop Length",
+            "Name Elevation",
+        ]
 
         # Indices where headers start, separating runs by difficulty
-        idx_headers = [i for i,j in enumerate(lst_rows) if j in lst_cols]
+        idx_headers = [i for i, j in enumerate(lst_rows) if j in lst_cols]
 
         # Create DataFrame from rows
-        df_trails = pd.DataFrame(lst_rows, columns=["trail_data"]).reset_index(drop=True)
-        
+        df_trails = pd.DataFrame(lst_rows, columns=["trail_data"]).reset_index(
+            drop=True
+        )
+
         # Expand DataFrame values into separate columns
         df_trails = df_trails["trail_data"].str.split("|", expand=True)
-        df_trails.columns = ["Trail Name", "Bottom Elev (ft)", "Top Elev (ft)", "Vertical Drop (ft)", "Length (mi)", "Blank"]
+        df_trails.columns = [
+            "Trail Name",
+            "Bottom Elev (ft)",
+            "Top Elev (ft)",
+            "Vertical Drop (ft)",
+            "Length (mi)",
+            "Blank",
+        ]
 
         df_trails.drop("Blank", axis=1, inplace=True)
 
@@ -113,23 +129,32 @@ class WebscrapeTrails:
         lst_difficulty = [i.replace("Ski runs: ", "") for i in lst_difficulty]
 
         df_difficulties = pd.DataFrame(lst_difficulty, index=idx_headers)
-        
-        df_combined = pd.merge(df_trails, df_difficulties,
-            left_index=True, right_index=True, how="outer")
 
-        df_combined.rename(columns={0:"Difficulty"}, inplace=True)
+        df_combined = pd.merge(
+            df_trails, df_difficulties, left_index=True, right_index=True, how="outer"
+        )
+
+        df_combined.rename(columns={0: "Difficulty"}, inplace=True)
 
         df_combined["Difficulty"].fillna(method="ffill", inplace=True)
 
         # Remove rows which are not trail names
-        df_combined = df_combined[~df_combined["Trail Name"].isin(lst_cols)].reset_index(drop=True)
+        df_combined = df_combined[
+            ~df_combined["Trail Name"].isin(lst_cols)
+        ].reset_index(drop=True)
 
         # Remove runs with no name and trails with __NA__ value
-        df_combined = df_combined[df_combined["Trail Name"].notnull()].reset_index(drop=True)
-        df_combined = df_combined[df_combined["Trail Name"] != self.blank_value].reset_index(drop=True)
+        df_combined = df_combined[df_combined["Trail Name"].notnull()].reset_index(
+            drop=True
+        )
+        df_combined = df_combined[
+            df_combined["Trail Name"] != self.blank_value
+        ].reset_index(drop=True)
 
         # Remove Lifts, Restaurants, and Terrain Park
-        df_combined = df_combined[~df_combined["Difficulty"].isin(["Lifts", "Restaurants", "terrain park"])].reset_index(drop=True)
+        df_combined = df_combined[
+            ~df_combined["Difficulty"].isin(["Lifts", "Restaurants", "terrain park"])
+        ].reset_index(drop=True)
 
         # Add URL
         df_combined["URL"] = URL
@@ -145,39 +170,41 @@ class WebscrapeTrails:
         OUTPUT
             Pandas DataFrame, with resort_name column altered
         """
-        
-        df["Resort"] = df["URL"].str.split("united-states-of-america/", 1, expand=True)[1]
-        df["Resort"] = df["Resort"].str.split("/", 1, expand=True)[0]
-        
-        dict_webscrape_trail_names = {'alpine-meadows': "Alpine Meadows",
-                            'arapahoe-basin': "Arapahoe Basin",
-                            'aspen-snowmass': "Aspen Snowmass",
-                            'bald-mountain': "Bald Mountain",
-                            'beaver-creek-resort': "Beaver Creek",
-                            'copper-mountain-resort': "Copper",
-                            'crested-butte-mountain-resort': "Crested Butte",
-                            'diamond-peak': "Diamond Peak",
-                            'eldora-mountain-resort': "Eldora",
-                            'jackson-hole': "Jackson Hole",
-                            'loveland-ski-area': "Loveland",
-                            'monarch-ski-area': "Monarch",
-                            'steamboat-ski-resort': "Steamboat",
-                            'taos-ski-valley': "Taos",
-                            'telluride-ski-resort': "Telluride",
-                            'vail-ski-resort': "Vail",
-                            'winter-park-resort': "Winter Park",
-                            'wolf-creek-ski-area': "Wolf Creek"}
 
-        df["Resort"] = df["Resort"].map(
-            dict_webscrape_trail_names).\
-            fillna(df["Resort"])
+        df["Resort"] = df["URL"].str.split("united-states-of-america/", 1, expand=True)[
+            1
+        ]
+        df["Resort"] = df["Resort"].str.split("/", 1, expand=True)[0]
+
+        dict_webscrape_trail_names = {
+            "alpine-meadows": "Alpine Meadows",
+            "arapahoe-basin": "Arapahoe Basin",
+            "aspen-snowmass": "Aspen Snowmass",
+            "bald-mountain": "Bald Mountain",
+            "beaver-creek-resort": "Beaver Creek",
+            "copper-mountain-resort": "Copper",
+            "crested-butte-mountain-resort": "Crested Butte",
+            "diamond-peak": "Diamond Peak",
+            "eldora-mountain-resort": "Eldora",
+            "jackson-hole": "Jackson Hole",
+            "loveland-ski-area": "Loveland",
+            "monarch-ski-area": "Monarch",
+            "steamboat-ski-resort": "Steamboat",
+            "taos-ski-valley": "Taos",
+            "telluride-ski-resort": "Telluride",
+            "vail-ski-resort": "Vail",
+            "winter-park-resort": "Winter Park",
+            "wolf-creek-ski-area": "Wolf Creek",
+        }
+
+        df["Resort"] = df["Resort"].map(dict_webscrape_trail_names).fillna(df["Resort"])
 
         # Drop URL column
         df.drop("URL", axis=1, inplace=True)
 
         # Reset index
         df = df.reset_index(drop=True)
-        
+
         return df
 
     def save_trail_data(self, df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
@@ -187,10 +214,13 @@ class WebscrapeTrails:
 
         current_date = date.today().strftime("%Y%m%d")
 
-        df.to_parquet(f"{self.CURRENT_DIRECTORY}/data/trail_data_{current_date}.parquet", index=False)
+        df.to_parquet(
+            f"{self.CURRENT_DIRECTORY}/data/trail_data_{current_date}.parquet",
+            index=False,
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     ws = WebscrapeTrails()
 
@@ -200,7 +230,7 @@ if __name__ == '__main__':
 
     # Combine trail data
     df_resorts = pd.concat(lst_trail_data)
-    
+
     # Capitalize trail difficulty
     df_resorts["Difficulty"] = df_resorts["Difficulty"].str.title()
 
@@ -208,20 +238,29 @@ if __name__ == '__main__':
     df_resorts = ws.rename_resorts(df=df_resorts)
 
     # Format trail values
-    lst_formatted_cols = ["Bottom Elev (ft)", "Top Elev (ft)", "Vertical Drop (ft)", "Length (mi)"]
+    lst_formatted_cols = [
+        "Bottom Elev (ft)",
+        "Top Elev (ft)",
+        "Vertical Drop (ft)",
+        "Length (mi)",
+    ]
     df_resorts[lst_formatted_cols] = df_resorts[lst_formatted_cols].astype("float64")
 
     # Convert run distance from miles to feet
-    df_resorts.loc[df_resorts["Length (mi)"] < 1, "Length (mi)"] = df_resorts["Length (mi)"] * 5280
-    
+    df_resorts.loc[df_resorts["Length (mi)"] < 1, "Length (mi)"] = (
+        df_resorts["Length (mi)"] * 5280
+    )
+
     # Format trail values as integers
     df_resorts[lst_formatted_cols] = df_resorts[lst_formatted_cols].astype(int)
 
     # Rename Length column
-    df_resorts.rename(columns={"Length (mi)":"Slope Length (ft)"}, inplace=True)
+    df_resorts.rename(columns={"Length (mi)": "Slope Length (ft)"}, inplace=True)
 
     # Calculate average steepness
-    df_resorts['Average Steepness'] = (df_resorts['Vertical Drop (ft)'] / df_resorts['Slope Length (ft)']).round(2)
+    df_resorts["Average Steepness"] = (
+        df_resorts["Vertical Drop (ft)"] / df_resorts["Slope Length (ft)"]
+    ).round(2)
 
     # Save trail data
     # ws.save_trail_data(df=df_resorts)
