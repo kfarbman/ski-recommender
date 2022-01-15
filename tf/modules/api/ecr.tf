2347dccd773d1f1137a977783355ece00c17f8de
Greg Chase <9953173+gregwchase@users.noreply.github.com>
2022-01-14 17:15:36 -0700

    Encrypt ECR with KMS

--- tf/modules/api/ecr.tf
+++ tf/modules/api/ecr.tf
@@ -2,6 +2,10 @@ resource "aws_ecr_repository" "ecr_repository" {
   name                 = "${var.product_name}-${var.environment}"
   image_tag_mutability = "IMMUTABLE"
 
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
+
   image_scanning_configuration {
     scan_on_push = true
   }
