36a4b60c49ae8ae56e116f8b76444d48facb610e
Greg Chase <9953173+gregwchase@users.noreply.github.com>
2022-01-18 17:04:32 -0700

    Specify VPC ID and subnet vars

--- tf/environments/development/main.tf
+++ tf/environments/development/main.tf
@@ -4,4 +4,6 @@ module "api" {
   source       = "../../modules/api"
   environment  = var.environment
   product_name = var.product_name
+  vpc_id       = var.vpc_id
+  alb_subnets  = var.alb_subnets
 }
