0e30be7037bba7d924b759505a5843164e11c350
Greg Chase <9953173+gregwchase@users.noreply.github.com>
2022-01-18 23:11:10 -0700

    Add vars for api module

--- tf/modules/api/vars.tf
+++ tf/modules/api/vars.tf
@@ -7,3 +7,13 @@ variable "product_name" {
   type        = string
   description = "Name of product"
 }
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID"
+}
+
+variable "alb_subnets" {
+  type        = list(any)
+  description = "ALB subnets"
+}
