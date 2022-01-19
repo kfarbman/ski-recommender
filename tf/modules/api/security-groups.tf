8ce0e729fccb7a6dc0f9b8fa84929c298ec66f88
Greg Chase <9953173+gregwchase@users.noreply.github.com>
2022-01-18 19:23:19 -0700

    Create security group for ALB and ECS access

---
+++ tf/modules/api/security-groups.tf
@@ -0,0 +1,36 @@
+resource "aws_security_group" "recsys_sg" {
+  name        = "${var.product_name}-${var.environment}-sg"
+  description = "Allow API traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Flask"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description      = "Allow all outbound access"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name        = "${var.product_name}-${var.environment}-sg"
+    Environment = var.environment
+  }
+}
+
