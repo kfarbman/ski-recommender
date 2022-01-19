2fd981e4e0ce0c5948b2ad0a3368536394fb71ab
Greg Chase <9953173+gregwchase@users.noreply.github.com>
2022-01-18 18:07:15 -0700

    [WIP] Create log groups and ECS service (failing)

--- tf/modules/api/ecs.tf
+++ tf/modules/api/ecs.tf
@@ -96,7 +96,33 @@ resource "aws_ecs_task_definition" "recsys_task_definition" {
 
   requires_compatibilities = ["FARGATE"]
 
-  task_role_arn      = aws_iam_role.ecs_task_role.arn
+  # task_role_arn      = aws_iam_role.ecs_task_role.arn
   execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
 
+}
+
+# BUG - Service not launching
+resource "aws_ecs_service" "reccys_ecs_service" {
+  name            = "${var.product_name}-${var.environment}-ecs-service"
+  cluster         = aws_ecs_cluster.recsys_cluster.id
+  task_definition = aws_ecs_task_definition.recsys_task_definition.arn
+  desired_count   = 1
+  iam_role        = aws_iam_role.ecs_task_execution_role.arn
+  depends_on      = [aws_iam_role_policy_attachment.ecs_task_execution_policy]
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.recsys_alb_target_group.arn
+    container_name   = "${var.product_name}-${var.environment}-container"
+    container_port   = 8080
+  }
+}
+
+resource "aws_cloudwatch_log_group" "recsys_log_group" {
+  name              = "/ecs/${var.product_name}-${var.environment}"
+  retention_in_days = 365
+}
+
+resource "aws_cloudwatch_log_stream" "recsys_log_stream" {
+  name           = "/ecs/${var.product_name}-${var.environment}-log-stream"
+  log_group_name = aws_cloudwatch_log_group.recsys_log_group.name
 }
