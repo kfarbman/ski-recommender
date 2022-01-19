7f4dca6814eb78fe1b68d985f2b63ea4dcc7b4b6
Greg Chase <9953173+gregwchase@users.noreply.github.com>
2022-01-18 17:14:48 -0700

    Create IAM roles and policies for ECS

--- tf/modules/api/ecs.tf
+++ tf/modules/api/ecs.tf
@@ -7,6 +7,56 @@ resource "aws_ecs_cluster" "recsys_cluster" {
   }
 }
 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${var.product_name}-${var.environment}-ecs-task-execution-role"
+
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Action" : "sts:AssumeRole",
+          "Principal" : {
+            "Service" : "ecs-tasks.amazonaws.com"
+          },
+          "Effect" : "Allow",
+          "Sid" : ""
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "${var.product_name}-${var.environment}-ecs-task-role"
+
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Action" : "sts:AssumeRole",
+          "Principal" : {
+            "Service" : "ecs-tasks.amazonaws.com"
+          },
+          "Effect" : "Allow",
+          "Sid" : ""
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_policy" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
 resource "aws_ecs_task_definition" "recsys_task_definition" {
   family = "${var.product_name}-${var.environment}-task-definition"
 
@@ -46,8 +96,7 @@ resource "aws_ecs_task_definition" "recsys_task_definition" {
 
   requires_compatibilities = ["FARGATE"]
 
-  # TODO: Create task and execution roles
-  # task_role_arn = "XXXX_TASK_ROLE_ARN_XXXX"
-  # execution_role_arn = "XXXX_EXECUTION_ROLE_ARN_XXXX"
+  task_role_arn      = aws_iam_role.ecs_task_role.arn
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
 
 }
